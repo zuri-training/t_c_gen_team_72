98c426d2062f2a9aa881a1766ced57182464eeb2
dev_ire <millenniumbest777@gmail.com>
2022-08-13 17:17:03 +0100

    'finalizing'

--- userauth/models.py
+++ userauth/models.py
@@ -21,70 +21,12 @@ class Terms_and_condition(models.Model):
             return self.body
 
 
-class UserModel(models.Model):
-    # the model is equivalent to using sql query  <<Create table UserModel(username varchar(100), email varchar(50), ..)
-    registration_date = models.DateTimeField('date published')
-    username = models.CharField(max_length=100)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    contactNumber = models.IntegerField(default=23480000000)
-    company = models.CharField(max_length=150, null=True)
-    userAddress=models.CharField(max_length=200, null=True)
-    businessType =(
-        ("CO", "Content Creator"),
-        ("BO", "Business Owner"),
-        ("EN", "Entrepreneur"),
-        ("GB", "Government Body"),
-        ("SP", "Software Product")
-    )
-    state= (
-        ("AB", "Abia"),
-        ("FC", "Abuja Federal Capital Territory"),
-        ("AD", "Adamawa"),
-        ("AK", "Akwa Ibom"),
-        ("AN", "Anambra"),
-        ("BA", "Bauchi"),
-        ("BY", "Bayelsa"),
-        ("BE", "Benue"),
-        ("BO", "Borno"),
-        ("CR", "Cross River"),
-        ("DE", "Delta"),
-        ("EB", "Ebonyi"),
-        ("ED", "Edo"),
-        ("EK", "Ekiti"),
-        ("EN", "Enugu"),
-        ("GO", "Gombe"),
-        ("IM", "Imo"),
-        ("JI", "Jigawa"),
-        ("KD", "Kaduna"),
-        ("KN", "Kano"),
-        ("KT", "Katsina"),
-        ("KE", "Kebbi"),
-        ("KO", "Kogi"),
-        ("KW", "Kwara"),
-        ("LA", "Lagos"),
-        ("NA", "Nasarawa"),
-        ("NI", "Niger"),
-        ("OG", "Ogun"),
-        ("ON", "Ondo"),
-        ("OS", "Osun"),
-        ("OY", "Oyo"),
-        ("PL", "Plateau"),
-        ("RI", "Rivers"),
-        ("SO", "Sokoto"),
-        ("TA", "Taraba"),
-        ("YO", "Yobe"),
-        ("ZA", "Zamfara")
-
-        #Tope is to compile all the state names since he is writing it manually. I no get strength abeg - Lola
-    )
-
-
-    def __str__(self):
-        return self.title
-
-#policy generator form - All question fields on UI
+# policy generator form - All question fields on UI
 class PrivacyPolicyQuestions(models.Model):
+    """
+    in our route '/question/3' the form field that takes in input the industry type returns a boolean value to the
+    fields in this model
+    """
     policyUseChoice = (
     ("Web", "Website"),
     ("App", "Mobile Application")
@@ -100,55 +42,6 @@ class PrivacyPolicyQuestions(models.Model):
     real_estate = models.BooleanField(null=True)
 
 
-    userCreateAccount=models.BooleanField(null=True)
-    websiteOffer=models.BooleanField(null=True)
-    personalInfo=(
-        ("NA", "Names"),
-        ("Phone No", "Phone Number"),
-        ("Email Ad", "Email Address"),
-        ("Mail Ad", "Mailing Address"),
-        ("Job", "Job Titles"),
-        ("User", "Usernames"),
-        ("Pass", "Passwords"),
-        ("Contact Pref", "Contact Preferences"),
-        ("Auth Data", "Authentication Data"),
-        ("Bill Ad", "Billing Addresses"),
-        ("Cred", "Credit Card Numbers")
-    )
-    provideService=models.BooleanField(null=True)
-    provideEmailAdd=models.CharField(max_length=50, null=True)
-    legalName=models.CharField(max_length=200, null=True)
-    tradeNameBusiness=models.BooleanField(null=True)
-    companyEmail=models.CharField(max_length=50, null=True)
-    phoneNumber=models.IntegerField(null=True)
-    fax=models.IntegerField(null=True)
-    country=(
-        ("AB", "Abia"),
-        ("AD", "Adamawa")
-        #Tope is to compile all the state names since he is writing it manually. I no get strength abeg - Lola
-    )
-    compAddressLine=models.CharField(max_length=200, null=True)
-    cityTown=models.CharField(max_length=150, null=True)
-    zipCode=models.IntegerField(null=True)
-    versionDate=models.DateField(null=True)
-
-
-def decision(self):
-    return self.policyUseChoice
-
-def decision(self):
-    return self.personalInfo
-
-def decision(self):
-    return self.planToUse
-
-def decision(self):
-    return self.country
-
-
-
-
-#I will continue if this is okay to proceed Ire. Please check.
 
 
 
@@ -156,30 +49,3 @@ def decision(self):
 
 
 
-#month_choice1 = (
-    #("JANUARY", "JAN1"),
-    #("FEBR", "FEB1"),
-    #("MARC", "MAR1"),
-    #("APRI", "APR1")
-#)
-#month_choice2 = (
- #   ("JANUARY", "JAN2"),
-  #  ("FEBRUARY", "FEB2"),
-   # ("MARCH", "MAR2"),
-    #("APRIL", "APR2")
-#)
-
-#def decides():
- #   return month_choice
-
-
-#def new_decides():
- #   return month_choice1
-
-
-#def newer_decides():
- #   return month_choice2
-
-#class Question(models.Model):
-   # question_text = models.CharField(max_length=200, choices=decides(), default='JANUARY')
-   # pub_date = models.DateTimeField('publication_date')
--- userauth/views.py
+++ userauth/views.py
@@ -22,8 +22,8 @@ from django.contrib import messages
 
 from django.http import HttpResponse
 
-
 from . import forms
+
 
 class CreateView(CreateView):  # Creates the view to insert text to database
     model = t_c_Db
@@ -43,13 +43,15 @@ class ListTheView(ListView):  # list the texts inserted into the database into t
             return redirect('%s?next=s%' % (settings.Login_url, request.path))
 
 
-#Omolola's code below -- tope please fix.
+# Omolola's code below -- tope please fix.
 def policyTest(request):
     return render(request, "policy.html")
+
 
 class home(TemplateView):
     template_name = "index.html"
 
+# resets the password
 @login_required
 def reset_password_view(request, pk):
     form = forms.ResetPassword()
@@ -62,53 +64,9 @@ def reset_password_view(request, pk):
             pass
     return render(request, 'reset_password.html', {'form': form})
 
-#Login authenticatin and request
 
-def new_login(request):
-    if request.method == 'POST':
+# controls the user signup
 
-        error = 0
-        # To check if username is not empty
-        if not request.POST['username']:
-            messages.error(request, 'Username field is required')
-
-
-        else:
-            username = request.POST['username']
-            print(username)
-
-        # To check if password is not empty
-        if not request.POST['password']:
-            messages.error(request, 'Password field is required')
-
-        else:
-            password = request.POST['password']
-
-        # if password and or password is null, redirected to login passage with erroe message
-
-        if error > 0:
-            return redirect('/login')
-
-        else:
-
-            user = auth.authenticate(username=username, password=password)
-
-            if user is not None:
-                auth.login(request, user)
-                messages.success(request, "You are successfully logged in")
-                return redirect('me')
-            else:
-               messages.error(request, 'Invalid Username or Password')
-               return render(request, 'loginform.html')
-    else:
-        if request.user.is_authenticated:
-            redirect("Home")
-        else:
-            return render(request, 'loginform.html')
-
-
-
-#Login authenticatin and request
 def signup(request):
     if request.method == "POST":
         error = 0
@@ -133,7 +91,6 @@ def signup(request):
         else:
             email = request.POST['email']
 
-
         # if password and or password is null, redirected to registration page with error message
 
         if error > 0:
@@ -149,7 +106,8 @@ def signup(request):
         else:
             return render(request, 'sign_up.html')
 
-#View for edit profile
+
+# View for edit profile
 @login_required
 def editProfile(request, id):
     if request.method == "POST":
@@ -163,92 +121,45 @@ def editProfile(request, id):
 
         if not request.POST['email']:
             messages.error(request, 'Email field is required')
-            error = error +1
+            error = error + 1
         else:
             email = request.POST['email']
 
         if not request.POST['company']:
             messages.error(request, 'company field is required')
-            error = error +1
+            error = error + 1
         else:
-            company  = request.POST['company']
+            company = request.POST['company']
 
         if not request.POST['business']:
             messages.error(request, 'Firstname field is required')
-            error = error +1
+            error = error + 1
         else:
             business = request.POST['business']
 
         if not request.POST['address']:
             messages.error(request, 'address field is required')
-            error = error +1
+            error = error + 1
         else:
             address = request.POST['address']
 
         if not request.POST['number']:
             messages.error(request, 'number field is required')
-            error = error +1
+            error = error + 1
         else:
             number = request.POST['number']
 
         if error > 0:
             return redirect("updateprofile")
         else:
-            #call the user detail from appropriate table using the users id e.g
-            myuser=User.objects.get(pk=id)
-            #input the above data into the database e.g myuser.company = company
+            # call the user detail from appropriate table using the users id e.g
+            myuser = User.objects.get(pk=id)
+            # input the above data into the database e.g myuser.company = company
             myuser.save()
             messages.success(request, "Your account has been updated successfully")
             return redirect('dashboard')
     else:
         return render(request, "editprofile.html")
-
-"""
-class CreateView(CreateView):  # Creates the view to insert text to database
-    model = t_c_Db
-    fields = [
-        "title", "description"
-    ]
-    template_name = 'landingPage.html'
-    success_url = 'list'
-
-
-class ListTheView(ListView):  # list the texts inserted into the database into the html file created here
-    model = t_c_Db
-    template_name = 'listView.html'
-
-    def my_view(self, request):
-        if not request.user.is_authenticated:
-            return redirect('%s?next=s%' % (settings.Login_url, request.path))
-
-
-# Create your views here.
-
-
-class UpdateTheView(UpdateView):  # list the texts inserted into the database into the html file created here
-<<<<<<< HEAD
-    model = Questions
-||||||| f761943
-    model = t_c_Db
-=======
-
-    model = t_c_Db
->>>>>>> 3130e815ab317f907f45821185e68001f3c9c9ce
-    fields = [
-        "question",
-    ]
-    template_name = 'Update.html'
-    success_url = '/'
-
-
-class DeleteTheView(DeleteView):
-    model = t_c_Db
-    template_name = 'Delete.html'
-    success_url = '/'
-    """
-
-
-# Create your views here.
 
 
 def reset_password_view(request, pk):
@@ -261,6 +172,7 @@ def reset_password_view(request, pk):
         if form.is_valid():
             pass
     return render(request, 'reset_password.html', {'form': form})
+
 
 """"
 def login(request):
@@ -283,7 +195,6 @@ def login(request):
         return render(request, 'loginform.html')
 
 """
-
 
 
 def signup(request):
@@ -317,94 +228,14 @@ class NewCreateView(CreateView):  # Creates the view to insert text to database
     template_name = 'create.html'
     success_url = 'list'
 
-class LoggedUser(TemplateView):
-    template_name = 'users.html'
-
-    def get(self, request):
-        form = forms.UserModelForm()
-        return render(request, self.template_name, {'form': form} )
-
-    def post(self, request):
-        if request.method == "POST":
-            form = forms.UserModelForm(request.POST)
-            if form.is_valid():
-                form.save()
-                reg_date = form.cleaned_data['reg_date']
-                username = form.cleaned_data['username']
-                email = form.cleaned_data['email']
-                password = form.cleaned_data['password']
-                contact_no = form.cleaned_data['contact_no']
-                company = form.cleaned_data['company']
-                user_address = form.cleaned_data['user_address']
-
-            args = {'form': form, 'reg_date': reg_date, 'username': username, 'email': email, 'password': password,
-                'contact_no': contact_no, 'company': company, 'user_address': user_address}
-        return redirect('/list', args)
-
-
-
-
-
-
-def post(self, request):
-    get_id = PrivacyPolicyQuestions.objects.get(pk=1)
-    if request.method == "POST":
-        other_policyUseChoice = request.POST['other_policyUseChoice']
-        userLocate = request.POST['userLocate']
-        userCreateAccount = request.POST['userCreateAccountl']
-        websiteOffer = request.POST['userCreateAccount']
-        reg_date = request.POST['reg_date']
-        username =request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        company = request.POST['company']
-        contact_no = request.POST['contact_no']
-        user_address = request.POST['user_address']
-        age = request.POST['age']
-        input_age = request.POST['exact_age']
-        containAds = request.POST['containAds']
-        upPostContent = request.POST['upPostConten']
-        disclosePerInfo = request.POST['disclosePerInfo']
-        wsecureMeausre = request.POST['wsecureMeausre']
-        country = request.POST['country']
-
-        user = PrivacyPolicyQuestions(registration_date=reg_date, username=username, email=email, password=password,
-                         company=company, user_address=user_address, contact_no=contact_no, age=age,
-                         input_age=input_age, containAds=containAds, upPostContent=upPostContent,
-                                      disclosePerInfo=disclosePerInfo, wsecureMeausre=wsecureMeausre, country=country)
-
-        user.save()
-        return redirect('me')
-    return render(request, 'users.html', {'get_id': get_id})
-
-
-def constuction(request):
-    value = request.POST['construction']
-    return value
-
-
-def finance(request):
-    value = request.POST['finance']
-    return value
-
-def health_care(request):
-    value = request.POST['health care']
-    return value
-
-def real_estate(request):
-    value = request.POST['real estate']
-    return value
-
-def employment(request):
-    value = request.POST['employment']
-    return value
 
 def question1(request):
     if request.method == "POST":
         construction = request.POST["browser"]
         if construction == "Construction":
             user = PrivacyPolicyQuestions(construction="True")
-#            user.save(update_fields=["construction"])
+
+
             user.save()
         elif construction == "Finance":
             user = PrivacyPolicyQuestions(finance="True")
@@ -431,8 +262,6 @@ def question1(request):
     return render(request, '1collection.html')
 
 
-
-
 def reset_password_view(request, pk):
     form = forms.ResetPassword()
 
@@ -454,7 +283,7 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('/list')
+            return redirect('dashboard')
         else:
             messages.info(request, 'Invalid Username or Password')
             return redirect('loginform')
@@ -466,9 +295,6 @@ def login(request):
         return render(request, 'loginform.html')
 
 
-
-
-
 def signup(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -477,7 +303,7 @@ def signup(request):
         user = User.objects.create_user(username, email, password)
         user.save()
         messages.success(request, 'success')
-        return redirect('/list')
+        return redirect('/me')
     return render(request, 'sign_up.html')
 
 
@@ -497,6 +323,8 @@ def question3(request):
     if request.method == "POST":
         construction = request.POST["browser"]
         if construction == "Construction":
+            user = PrivacyPolicyQuestions.objects.get(id=3)
+            print(user)
             user = PrivacyPolicyQuestions(construction="True")
             #            user.save(update_fields=["construction"])
             user.save()
@@ -531,3 +359,21 @@ def question4(request):
 
 def generate(request):
     return render(request, 'generate.html')
+
+
+def dashboard(request):
+    return render(request, 't-c-preview.html')
+
+
+def about_us(request):
+    return render(request, 'about-us.html')
+
+
+def faq(request):
+    return render(request, 'security_faqs.html')
+
+
+def policy(request):
+    return render(request, 'policy.html')
+
+
